31e706f71a320abee63c0716c026930c15ecbcf0
Rahul <rahulzandennavar99@gmail.com>
2023-08-03 13:27:54 +0530

    Initial

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
+
+
+const DashbordPage = () => {
+  return (
+   <>
+   <div>
+   <p>
+    Dashboard Page (Protected)
+
+   </p>
+   </div>
+   </>
+  )
+}
+
+export default DashbordPage;
