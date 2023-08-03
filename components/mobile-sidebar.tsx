a5bdb7cfbb7abdb77d49c5719f5e4e762cfdad9e
Rahul <rahulzandennavar99@gmail.com>
2023-08-03 19:57:35 +0530

    Dashboard  & Conversation AI UI

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -8,8 +8,23 @@ import { Sheet,
          SheetTrigger 
         } from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
+import { useEffect, useState } from "react";
+
 
 const MobileSidebar = () => {
+
+ {/* To fix hydration Error*/}
+  const [isMounted , setIsMounted] = useState(false);
+
+  useEffect(() =>{
+    setIsMounted(true);
+  } , []);
+
+if(!isMounted)
+  {
+    return null;
+  }
+
   return (
     <Sheet>
       <SheetTrigger>
